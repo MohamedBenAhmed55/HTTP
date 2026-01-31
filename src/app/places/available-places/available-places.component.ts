f6f7f4391429b12322f551834544104dae9dc8c5
waelkhayati <khayatiwael@gmail.com>
2026-01-31 17:34:46 +0100

    Transforming & Using Response Data

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -20,15 +21,21 @@ export class AvailablePlacesComponent implements OnInit {
   // constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
-    const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places'
+    const subscription = this.httpClient
+    .get<{places: Place[]}>('http://localhost:3000/places'
     //   , {
     //   observe: 'response'
     //   observe: 'events'
     // }
-  ).subscribe(({
-      next: (resData) => {
-        console.log(resData);
+  )
+  .pipe(
+    map((resData) => resData.places)
+  )
+  .subscribe(({
+      next: (places) => {
+        // console.log(resData);
         // console.log(response.body?.places);
+        this.places.set(places);
       }
     }));
     
