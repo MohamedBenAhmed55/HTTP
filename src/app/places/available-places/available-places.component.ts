6af7f05d9fca4dd339f31189662f4d3f857e1738
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-02-01 23:21:22 +0100

    Handling HTTP Errors

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -16,37 +16,50 @@ import { map } from 'rxjs';
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
+  error = signal('');
   private httpClient = inject(HttpClient);
-  private destroyRef = inject(DestroyRef)
-  
+  private destroyRef = inject(DestroyRef);
+
   // constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
     this.isFetching.set(true);
     const subscription = this.httpClient
-    .get<{places: Place[]}>('http://localhost:3000/places'
-    //   , {
-    //   observe: 'response'
-    //   observe: 'events'
-    // }
-  )
-  .pipe(
-    map((resData) => resData.places)
-  )
-  .subscribe(({
-      next: (places) => {
-        // console.log(resData);
-        // console.log(response.body?.places);
-        this.places.set(places);
-      },
-      complete: () => {
-        this.isFetching.set(false);
-      }
-    }));
-    
+      .get<{ places: Place[] }>(
+        'http://localhost:3000/places',
+        //   , {
+        //   observe: 'response'
+        //   observe: 'events'
+        // }
+      )
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error)
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching the available places. Please try again later.',
+              ),
+          );
+        }),
+      )
+      .subscribe({
+        next: (places) => {
+          // console.log(resData);
+          // console.log(response.body?.places);
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+      });
+
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
-
   }
 }
